02bfec44a48dea577e445552eee847412489893f
Sam Tebbs <samuel.tebbs@gmail.com>
2017-11-10 13:28:49 +0000

    Added "switch" and "branch" commands

--- src/main/scala/com/github/samtebbs33/baldr/Command.scala
+++ src/main/scala/com/github/samtebbs33/baldr/Command.scala
@@ -13,10 +13,29 @@ object Command {
     "add" -> new Command(1, a => Baldr.stage(a(0))),
     "rm" -> new Command(1, a => Baldr.unstage(a(0))),
     "ignore" -> new Command(1, a => Baldr.ignore.add(a(0))),
-    "unignore" -> new Command(1, a => Baldr.ignore.remove(a(0)))
+    "unignore" -> new Command(1, a => Baldr.ignore.remove(a(0))),
+    "switch" -> new Command(1, a => {
+      val branchName = a(0)
+      Branch.getBranch(branchName) match {
+        case None => println(s"Branch '$branchName' doesn't exist")
+        case Some(branch) => Branch.switchBranch(branch)
+      }
+    }),
+    "branch" -> new Command(1, a => {
+      val branchName = a(0)
+      Branch.branchExists(branchName) match {
+        case true => println(s"The branch '$branchName' already exists")
+        case false => {
+          val head = if (a.length > 1) a(1) else Branch.current.head
+          Branch.setCurrentBranch(Branch.createBranch(branchName, head))
+          // If the new branch head is not the same as the current head, then revert back to that head
+          if(!head.equals(Branch.current.head)) Branch.switchBranch(Branch.current)
+        }
+      }
+    })
   )
 
-  def accept(cmd: String, args: Array[String]) = commandMap.get(cmd) match {
+  def accept(cmd: String, args: Array[String]): Unit = commandMap.get(cmd) match {
     case Some(value) if args.length >= value.minArgCount => value.handler(args)
     case Some(value) => println(s"The command '$cmd' expects at least ${value.minArgCount} argument(s)")
     case None => println("No such command found")
